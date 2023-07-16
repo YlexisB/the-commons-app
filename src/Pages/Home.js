32694409694056c9774aee8916d2801c90504901
Ylexis Butler <ylexislauren@gmail.com>
2023-07-16 12:27:58 -0400

    file cleanup

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Components/Header";
 import WeeklyStory from "../Components/WeeklyStory";
 import Footer from "../Components/Footer";
-import Aos from "aos";
+
 import "aos/dist/aos.css";
 const Home = () => {
   return (
